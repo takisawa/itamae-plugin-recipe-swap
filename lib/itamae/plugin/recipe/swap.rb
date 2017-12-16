016415e821f303080d46d6da2b1d27347dfacfc2
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-12-17 07:41:07 +0900

    Add gem templates.

---
+++ lib/itamae/plugin/recipe/swap.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/swap/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Swap
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/itamae/plugin/recipe/swap_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Itamae::Plugin::Recipe::Swap do
+  it "has a version number" do
+    expect(Itamae::Plugin::Recipe::Swap::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
