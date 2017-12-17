f501d3796473b3adb56fdb094bb14d7a6afb4318
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-12-17 13:42:43 +0900

    Fix checking script exists swap file.

--- lib/itamae/plugin/recipe/swap/default.rb
+++ lib/itamae/plugin/recipe/swap/default.rb
@@ -12,10 +12,10 @@ swap_size = node[:swap][:size]
 
 execute 'Setup swap file' do
   command "fallocate -l #{swap_size} #{swap_path} && chmod 600 #{swap_path} && mkswap /swapfile; swapon #{swap_path}"
-  not_if 'swapon | grep ^/swapfile'
+  not_if "swapon | grep '^#{path}\s'"
 end
 
 execute 'Add swap record to /etc/fstab' do
   command "echo '#{swap_path} none swap sw 0 0' >> /etc/fstab"
-  not_if "grep ^#{swap_path} /etc/fstab"
+  not_if "grep '^#{swap_path}\s' /etc/fstab"
 end
