86f2fd8594a10d6c2ee90431c37101441409609f
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-12-17 09:59:37 +0900

    Can specify swap path and size by node.yml.

--- lib/itamae/plugin/recipe/swap/default.rb
+++ lib/itamae/plugin/recipe/swap/default.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+node.reverse_merge!(
+  swap: {
+    path: '/swapfile',
+    size: '512M'
+  },
+)
+
+swap_path = node[:swap][:path]
+swap_size = node[:swap][:size]
+
 execute 'Setup swap file' do
-  command 'fallocate -l 1G /swapfile; chmod 600 /swapfile; mkswap /swapfile; swapon /swapfile'
+  command "fallocate -l #{swap_size} #{swap_path} && chmod 600 #{swap_path} && mkswap /swapfile; swapon #{swap_path}"
   not_if 'swapon | grep ^/swapfile'
 end
 
 execute 'Add swap record to /etc/fstab' do
-  command 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
-  not_if 'grep ^/swapfile /etc/fstab'
+  command "echo '#{swap_path} none swap sw 0 0' >> /etc/fstab"
+  not_if "grep ^#{swap_path} /etc/fstab"
 end
